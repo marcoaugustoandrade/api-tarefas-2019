242621b028c74bf23a313b420f439644e5a16eb3
evaldojr100 <evaldojr_melo@hotmail.com>
2019-10-23 21:24:46 -0400

    inserçao com validacao

--- src/routes/categoriaRoute.js
+++ src/routes/categoriaRoute.js
@@ -2,8 +2,9 @@ const express = require ('express')
 const router = express.Router()
 const categoriaController = require('../controllers/categoriaController')
 const apiController = require('../controllers/apiController')
+const categoriaValidation = require ('../util/categoriaValidation')
 
-router.post('/',apiController.verifica,categoriaController.inserir)
+router.post('/',apiController.verifica,categoriaValidation.inserir,categoriaController.inserir)
 
 
 module.exports = router
--- src/util/categoriaValidation.js
+++ src/util/categoriaValidation.js
@@ -1,6 +1,6 @@
 const { check } = require ('express-validator')
 
 exports.inserir = [
-    check('descricao').exists().trim().withMessage('A descrição não pode estar em branco.'),
-    check('cor').exists().isHexadecimal().withMessage('A cor informada precisa ser em formato Hexadecimal.')
+    check('descricao').exists().trim().isLength({min:1}).withMessage('A descrição não pode estar em branco.'),
+    check('cor').exists().isHexColor().withMessage('A cor informada precisa ser em formato Hexadecimal.')
 ]
