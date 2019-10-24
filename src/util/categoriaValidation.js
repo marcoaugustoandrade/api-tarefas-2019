264bff0040076d50868c90557532cd719e4a23ca
jhonifigueredo <jhoni_choc@hotmail.com>
2019-10-23 21:58:09 -0400

    Segunda alteração

--- src/util/categoriaValidation.js
+++ src/util/categoriaValidation.js
@@ -2,10 +2,9 @@ const { check } = require('express-validator')
 
 exports.alterar = [
   check('id')
-  .exists().withMessage('O id não pode estar em branco')
+  .exists().isLength({min:1}).withMessage('O id não pode estar em branco')
   .isInt().withMessage('O id deve ser um número inteiro'),
-  check('descricao').exists().trim().withMessage('A descrição não pode estar em branco'),
-  check('cor').exists().withMessage('A cor não pode estar em branco')
+  check('descricao').exists().trim().isLength({min:1}).withMessage('A descrição não pode estar em branco')
 ]
 
 
