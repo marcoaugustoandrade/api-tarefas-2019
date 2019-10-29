2b25d2ecdaefb8e1d32ec386f32c57567f3c8444
Willian Santos <desenvolvimento@grupoib3.com.br>
2019-10-28 21:50:10 -0400

    Adicionando validation para Data da Tarefas

--- src/routes/categoriaRoute.js
+++ src/routes/categoriaRoute.js
@@ -6,5 +6,7 @@ const categoriaValidation = require('../util/categoriaValidation')
 
 
 route.get('/:id', apiController.verifica, categoriaValidation.listarPorId, categoriaController.listarPorId)
+route.post('/', apiController.verifica, categoriaValidation.inserir, categoriaController.inserir)
+route.put('/:id', apiController.verifica, categoriaValidation.alterar, categoriaController.alterar)
 
 module.exports = route
--- src/util/categoriaValidation.js
+++ src/util/categoriaValidation.js
@@ -7,4 +7,17 @@ exports.listarPorId = [
   .isInt().withMessage('O id deve ser um número inteiro')
 ]
 
+exports.inserir = [
+  check('data')
+  .exists.trim().withMessage('O campo data da categoria não pode ser vazio')
+  .isLength({ min: 8 }).withMessage('A data deve ser conter no minimo 8 caracteres')
+  .isISO8601().withMessage('A data deve estar no formato valido')
+]
+
+exports.alterar = [
+  check('data')
+  .exists.trim().withMessage('O campo data da categoria não pode ser vazio')
+  .isLength({ min: 8 }).withMessage('A data deve ser conter no minimo 8 caracteres')
+  .isISO8601().withMessage('A data deve estar no formato valido')
+]
 
