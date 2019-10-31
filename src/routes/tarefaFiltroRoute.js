d8c0a04e1ba632f9cf0191e9dc1db02414b811c6
desenvolvimento2.ib3 <desenvolvimento2@grupoib3.com.br>
2019-10-30 20:21:06 -0400

    Ajuste Tarefa Route

---
+++ src/routes/tarefaFiltroRoute.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const route = express.Router()
+const tarefaController = require('../controllers/tarefaController')
+const tarefaValidation = require('../util/tarefaValidation')
+const apiController = require('../controllers/apiController')
+
+route.get('/descri/?'       ,   apiController.verificar, tarefaValidation.listarPorDescricao, tarefaController.listarPorDescricao)
+route.get('/datas/?'        ,   apiController.verificar, tarefaValidation.listarPorDatas    , tarefaController.listarPorDatas)
+route.get('/data/?'         ,   apiController.verificar, tarefaValidation.listarPorDt       , tarefaController.listarPorDt)
+route.get('/pagination/?'   ,   apiController.verificar, tarefaController.listarperpage)
+
+
+module.exports = route
