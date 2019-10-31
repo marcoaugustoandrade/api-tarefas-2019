aae393bba6dae4ecb37539d53ce44d4b780ee8db
desenvolvimento2.ib3 <desenvolvimento2@grupoib3.com.br>
2019-10-30 21:19:43 -0400

    Ajuste schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,7 @@
 create database if not exists db_tarefas;
 use db_tarefas;
 
-create table usuarios(
+create table if not exists usuarios(
   id      int             not null  auto_increment,
   email   varchar(200)    not null,  
   senha   varchar(200)    not null,
@@ -14,7 +14,7 @@ create table usuarios(
 insert into usuarios (id, email, senha) values
   (1, 'suporte@gmail.com', '$2b$10$6ZbT4sKq5St91o5bgpJpReNDHci.a3lQ18ObJAE97ljgApqFLyvNW');
 
-create table categorias(
+create table if not exists categorias(
   id          int           not null  auto_increment,
   descricao   varchar(200)  not null,
   cor         varchar(9),
@@ -32,7 +32,7 @@ insert into categorias (id, descricao, cor, id_user) values
   (3, "Trabalho de Conclusão", "#F2C94C", 1),
   (4, "Trabalho", "#EB5757", 1);
 
-create table tarefas(
+create TABLE if not exists tarefas(
 
   id            int           not null  auto_increment,
   descricao     varchar(255)  not null,
@@ -45,19 +45,19 @@ create table tarefas(
   id_tarefa     int,
 
   primary key (id),
-  foreign key (categoria_id) references categorias (id)
-  foreign key (id_user) references usuarios(id)
+  foreign key (categoria_id) references categorias (id),
+  foreign key (id_user) references usuarios(id),
   foreign key (id_tarefa) references tarefas(id)
 );
-insert into tarefas (descricao, data, realizado, categoria_id) values 
+insert into tarefas (descricao, data, realizado, categoria_id,id_user) values 
 
-  ('Pagar conta de energia', '2018-10-03 10:00:00', false, 1,),
-  ('Inciar o trabalho de ED', '2018-10-03 12:00:00', false, 2),
-  ('Pagar conta de energia', '2018-10-03 10:0', false, 1),
-  ('Inciar o trabalho', '2018-10-03 12:00:00', false, 2),
-  ('Abastecer o carro', '2018-10-04 00:00:00', true, 1),
-  ('Pagar conta de água', '2018-11-12 10:00:00', true, 1),
-  ('Entregar trabalho de ED', '2018-11-14 19:00:00', false, 2),
-  ('Entregar trabalho de Meio Ambiente', '2018-11-15 19:00:00', false, 2),
-  ('Verificar folha de ponto', '2018-12-13 19:00:00', false, 4),
-  ('Entregar capítulo introducao', '2018-11-28 19:00:00', false, 3);
+  ('Pagar conta de energia', '2018-10-03 10:00:00', false, 1, 1),
+  ('Inciar o trabalho de ED', '2018-10-03 12:00:00', false, 2, 1),
+  ('Pagar conta de energia', '2018-10-03 10:0', false, 1, 1),
+  ('Inciar o trabalho', '2018-10-03 12:00:00', false, 2, 1),
+  ('Abastecer o carro', '2018-10-04 00:00:00', true, 1, 1),
+  ('Pagar conta de água', '2018-11-12 10:00:00', true, 1, 1),
+  ('Entregar trabalho de ED', '2018-11-14 19:00:00', false, 2, 1),
+  ('Entregar trabalho de Meio Ambiente', '2018-11-15 19:00:00', false, 2, 1),
+  ('Verificar folha de ponto', '2018-12-13 19:00:00', false, 4, 1),
+  ('Entregar capítulo introducao', '2018-11-28 19:00:00', false, 3, 1);
